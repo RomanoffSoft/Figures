79e894af258fae54344a5e80a9296394552f4590
_ext Romanov <sergey.romanov.ext@wincor-nixdorf.com>
2018-03-09 21:29:41 +0300

    Init

---
+++ Figures/Figure.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    public abstract class Figure
+    {
+        public abstract double CalcArea();
+    }
+
+    public class Triangle : Figure
+    {
+        double _sideA, _sideB, _sideC;
+
+        public double SideA { get { return _sideA; } }
+        public double SideB { get { return _sideB; } }
+        public double SideC { get { return _sideC; } }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("Incorrect input parameters");
+            }
+
+            if (sideA + sideB < sideC ||
+                sideA + sideC < sideB ||
+                sideB + sideC < sideA)
+            {
+                throw new ArgumentException("Triangle with such sides does not exist");
+            }
+
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public override double CalcArea()
+        {
+            //Geron`s formula
+            double p = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+    }
+
+    //прямоугольный треугольник
+    public class RightTriangle : Triangle
+    {
+        public RightTriangle(double sideA, double sideB, double sideC)
+            : base(sideA, sideB, sideC)
+        {
+            //check if triangle actually right
+            if (!IsTriangleRight())
+            {
+                throw new ArgumentException("Triangle cannot be right with such side lengths");
+            }
+        }
+
+        private bool IsTriangleRight()
+        {
+            List<double> Sides = new List<double>() { SideA, SideB, SideC };
+            Sides.Sort();
+            if (Sides[2] * Sides[2] == Sides[0] * Sides[0] + Sides[1] * Sides[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public override double CalcArea()
+        {
+            List<double> Sides = new List<double>() { SideA, SideB, SideC };
+            Sides.Sort();
+            return Sides[0] * Sides[1] * 0.5;
+        }
+
+    }
+    public class Circle : Figure
+    {
+        private double _radius;
+        public double Radius { get { return _radius; } }
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public override double CalcArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+
+}
---
+++ TestFigures/TestFiguresAreas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Figure;
+
+namespace TestFigures
+{
+    [TestClass]
+    public class TestFiguresAreas
+    {
+        [TestMethod]
+        public void TestTriangleArea()
+        {
+            Triangle triangle = new Triangle(6, 8, 10);
+            double expected = 24;
+            double actual = triangle.CalcArea();
+            Assert.AreEqual(expected, actual, "Area not calcaulated correctly");
+        }
+        [TestMethod]
+        public void TestRightTriangleArea()
+        {
+            RightTriangle rtriangle = new RightTriangle(3, 4, 5);
+            double expected = 6;
+            double actual = rtriangle.CalcArea();
+            Assert.AreEqual(expected, actual, "Area not calculated correctly");
+        }
+        [TestMethod]
+        public void TestCircleArea()
+        {
+            Circle circle = new Circle(8);
+            double expected = 201.0619;
+            double actual = Math.Round(circle.CalcArea(),4);
+            Assert.AreEqual(expected,actual,"Area not calculated correctly");
+        }
+    }
+}
