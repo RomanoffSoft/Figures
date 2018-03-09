cbbaea8cd680126af1477125ccf0cdcf4aede190
_ext Romanov <sergey.romanov.ext@wincor-nixdorf.com>
2018-03-09 21:46:08 +0300

    Added demo project

---
+++ FigureDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure tri = new Triangle(6, 8, 10);
+            Figure tri2 = new Triangle(7, 10, 12);
+            Figure crc = new Circle(5);
+            List<Figure> Figures = new List<Figure> { tri,tri2, crc };
+            Console.WriteLine(CalcAreaOfFigures(Figures));
+        }
+        // Этот метод ничего не знает о том, какие именно фигуры 
+        // находятся в коллекции figures , но считает их площадь
+        static double CalcAreaOfFigures(List<Figure> figures)
+        {
+            double area = 0;
+            foreach (Figure figure in figures)
+            {
+                area += figure.CalcArea();
+            }
+            return area;
+        }
+    }
+}
--- Figures/Figure.cs
+++ Figures/Figure.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Figure
+namespace Figures
 {
     public abstract class Figure
     {
